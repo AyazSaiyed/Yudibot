600ce68206a8bddc4194e3461079de4215113240
Ayaz Saiyed <49748587+AyazSaiyed@users.noreply.github.com>
2021-05-19 13:34:39 +0530

    Updated static files path

--- core/settings.py
+++ core/settings.py
@@ -111,19 +111,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-#############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/ubuntu/Yudibot-master/static/'
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
+# Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'core/static'),
+   os.path.join(BASE_DIR, 'static'),
 )
 #############################################################
 #############################################################
